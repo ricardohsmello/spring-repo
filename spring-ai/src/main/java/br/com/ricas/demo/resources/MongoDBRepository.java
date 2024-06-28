e58eccaa49415439aff1f8776c4866195a4f10bf
Ricardo <ricardohsmello@gmail.com>
2024-06-27 23:59:32 -0300

    adding mongodb sync

--- spring-ai/src/main/java/br/com/ricas/demo/resources/MongoDBRepository.java
+++ spring-ai/src/main/java/br/com/ricas/demo/resources/MongoDBRepository.java
@@ -3,4 +3,8 @@ package br.com.ricas.demo.resources;
 public class MongoDBRepository {
 
     private String collectionName;
+
+    public String getCollectionName() {
+        return collectionName;
+    }
 }

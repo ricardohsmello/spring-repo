47777cdbf8e266399fc4ac11a657481ffd97f7d8
Ricardo <ricardohsmello@gmail.com>
2024-07-06 01:38:39 -0300

    creating project structure

--- spring-data-mongodb/src/main/java/br/com/ricas/sample/application/config/AppConfig.java
+++ spring-data-mongodb/src/main/java/br/com/ricas/sample/application/config/AppConfig.java
@@ -1,16 +1,23 @@
 package br.com.ricas.sample.application.config;
 
+import br.com.ricas.sample.domain.ProductServiceImpl;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.core.MongoClientFactoryBean;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public MongoClientFactoryBean mongo() {
-       MongoClientFactoryBean factory = new MongoClientFactoryBean();
-        factory.setHost("localhost");
-        return factory;
+    MongoClient mongoClient() {
+        return MongoClients.create("mongodb://localhost:27017");
+    }
+
+    @Bean
+    MongoOperations mongoTemplate(MongoClient mongoClient) {
+        return new MongoTemplate(mongoClient, "my_database");
     }
 }
